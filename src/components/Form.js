55044359c5295ae21a4ddcda6745e832bd695e0d
Gabi-data <gabriellyvitoria18g@gmail.com>
2026-04-13 16:09:12 -0300

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useRef } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import styled from "styled-components";
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Label = styled.label``;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #BC2A8D;
+  color: white;
+  height: 42px;
+`;
+
+const Form = ({ onEdit, setOnEdit, getUsers }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current;
+
+      user.nome.value = onEdit.nome;
+      user.email.value = onEdit.email;
+      user.user.value = onEdit.user;
+      user.data_nascimento.value = onEdit.data_nascimento;
+      user.password_hash.value = onEdit.password_hash;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = ref.current;
+
+    if (
+      !user.nome.value ||
+      !user.email.value ||
+      !user.user.value ||
+      !user.data_nascimento.value ||
+      !user.password_hash.value
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          nome: user.nome.value,
+          email: user.email.value,
+          user: user.user.value,
+          data_nascimento: user.data_nascimento.value,
+          password_hash: user.password_hash.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800/", {
+          nome: user.nome.value,
+          email: user.email.value,
+          user: user.user.value,
+          data_nascimento: user.data_nascimento.value,
+          password_hash: user.password_hash.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.nome.value = "";
+    user.email.value = "";
+    user.user.value = "";
+    user.data_nascimento.value = "";
+    user.password_hash.value = "";
+
+    setOnEdit(null);
+    getUsers();
+  };
+
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type="email" />
+      </InputArea>
+      <InputArea>
+        <Label>Username</Label>
+        <Input name="user" />
+      </InputArea>
+      <InputArea>
+        <Label>Data de Nascimento</Label>
+        <Input name="data_nascimento" type="date" />
+      </InputArea>
+      <InputArea>
+        <Label>Senha</Label>
+        <Input name="password_hash" type="password" />
+      </InputArea>
+      <Button type="submit">SALVAR</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
---
+++ src/components/Grid.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import { FaTrash, FaEdit } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+const Table = styled.table`
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+  max-width: 800px;
+  margin: 20px auto;
+  word-break: keep-all;
+`;
+
+export const Thead = styled.thead``;
+export const Tbody = styled.tbody``;
+export const Tr = styled.tr``;
+
+export const Th = styled.th`
+  text-align: start;
+  border-bottom: inset;
+  padding-bottom: 5px;
+  padding-right: 20px;
+  white-space: nowrap;
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+export const Td = styled.td`
+  padding-top: 15px;
+  padding-right: 20px;
+  white-space: nowrap;
+  text-align: ${(props) => (props.aligncenter ? "center" : "start")};
+  width: ${(props) => (props.width ? props.width : "auto")};
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+const formatDate = (dateString) => {
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  return date.toLocaleDateString("pt-BR");
+};
+
+const Grid = ({ users, setUsers, setOnEdit, getUsers }) => {  {/* ✅ apenas uma declaração */}
+
+  const handleEdit = (item) => {   {/* ✅ era handleEditt com dois t */}
+    setOnEdit(item);
+  };
+
+  const handleDelete = async (id) => {
+    await axios
+      .delete("http://localhost:8800/" + id)
+      .then(({ data }) => {
+        toast.success(data);
+        getUsers();
+      })
+      .catch(({ data }) => toast.error(data));
+
+    setOnEdit(null);
+  };
+
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Nome</Th>
+          <Th>Email</Th>
+          <Th>User</Th>
+          <Th onlyWeb>Data de Nascimento</Th>
+          <Th onlyWeb>Password</Th>
+          <Th>Editar</Th>
+          <Th>Excluir</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {users.map((item, i) => (
+          <Tr key={i}>
+            <Td width="15%">{item.nome}</Td>
+            <Td width="25%">{item.email}</Td>
+            <Td width="15%">{item.user}</Td>
+            <Td width="15%" onlyWeb>{formatDate(item.data_nascimento)}</Td>
+            <Td width="15%" onlyWeb>{item.password}</Td>
+            <Td aligncenter width="5%">
+              <FaEdit onClick={() => handleEdit(item)} />   {/* ✅ nome correto */}
+            </Td>
+            <Td aligncenter width="5%">
+              <FaTrash onClick={() => handleDelete(item.id)} />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
+
+export default Grid;
